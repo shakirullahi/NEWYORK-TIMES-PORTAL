1c945f689e30add95a163e72b680c735cb7eaada
Sakirullahi <opshaktvl@gmail.com>
2021-08-14 17:59:02 +0530

    articles context setup

--- src/context/article/ArticleState.js
+++ src/context/article/ArticleState.js
@@ -2,14 +2,73 @@ import React, { useReducer } from "react";
 import axios from "axios";
 import ArticleContext from "./articleContext";
 import articleReducer from "./articleReducer";
+import {
+  GET_ARTICLES_SUCCESS,
+  GET_SECTIONS_SUCCESS,
+  // FILTER_ARTICLES_SUCCESS,
+} from "../types";
 
 const ArticleState = (props) => {
-  const initialState = {};
+  const initialState = {
+    articles: null,
+    sections: null,
+    section: null,
+    currentArticlePage: 1,
+  };
+
+  //get articles
+  const getArticles = (section, currentArticlePage) => {
+    if (!section) {
+      section = "all";
+    }
+    let offset = (currentArticlePage - 1) * 20 + 1;
+    try {
+      const res = await axios.get(
+        `https://api.nytimes.com/svc/news/v3/content/all/${section}.json?limit=20?offset=${offset}?api-key=uR1j3A82i48Cvvn6A4pQRWBCIhUCIvG7`
+      );
+      console.log(res.data.results);
+      dispatch({
+        type: GET_ARTICLES_SUCCESS,
+        payload: {
+          articles: res.data.results,
+          section,
+          currentArticlePage,
+        },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  //get sections
+  const getSections = () => {
+    try {
+      const res = await axios.get(
+        `https://api.nytimes.com/svc/news/v3/content/section-list.json?api-key=uR1j3A82i48Cvvn6A4pQRWBCIhUCIvG7`
+      );
+      console.log(res.data.results);
+      dispatch({
+        type: GET_SECTIONS_SUCCESS,
+        payload: res.data.results,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   const [state, dispatch] = useReducer(articleReducer, initialState);
 
   return (
-    <ArticleContext.Provider value={{}}>
+    <ArticleContext.Provider
+      value={{
+        articles: state.articles,
+        sections: state.sections,
+        section: state.section,
+        currentArticlePage: state.currentArticlePage,
+        getArticles,
+        getSections,
+      }}
+    >
       {props.children}
     </ArticleContext.Provider>
   );
--- src/context/article/articleReducer.js
+++ src/context/article/articleReducer.js
@@ -1,5 +1,19 @@
+import { GET_ARTICLES_SUCCESS, GET_SECTIONS_SUCCESS } from "../types";
+
 const articleReducer = (state, action) => {
   switch (action.type) {
+    case GET_ARTICLES_SUCCESS:
+      return {
+        ...state,
+        articles: action.payload.articles,
+        section: action.payload.section,
+        currentArticlePage: action.payload.currentArticlePage,
+      };
+    case GET_SECTIONS_SUCCESS:
+      return {
+        ...state,
+        sections: action.payload,
+      };
     default:
       return state;
   }
