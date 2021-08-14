a6a84f97a442cc8c022fd641eb38c34866695c98
Sakirullahi <opshaktvl@gmail.com>
2021-08-14 17:13:32 +0530

    userContext setup

--- src/context/user/UserState.js
+++ src/context/user/UserState.js
@@ -4,15 +4,17 @@ import userReducer from "./userReducer";
 
 import {
   REGISTER_SUCCESS,
-  REGISTER_FAIL,
   LOGIN_SUCCESS,
-  LOGIN_FAIL,
+  LOGOUT,
+  PROFILE_UPDATE_SUCCESS,
+  ACCOUNT_DELETE_SUCCESS,
+  ADD_READ_LATER_SUCCESS,
+  REMOVE_READ_LATER_SUCCESS,
 } from "../types";
 
 const UserState = (props) => {
   const initialState = {
     user: null,
-    readLaterList: [],
     currentReadLaterPage: 1,
   };
 
@@ -38,21 +40,157 @@ const UserState = (props) => {
         // Re-serialize the array back into a string and store it in localStorage
         localStorage.setItem("users", JSON.stringify(users));
 
-        dispatch({ type: REGISTER_SUCCESS, payload: "" });
+        formData.readLaterList = [];
+        delete formData.password;
+
+        dispatch({ type: REGISTER_SUCCESS, payload: formData });
       }
     } catch (err) {
       console.log(err);
-      dispatch({ type: REGISTER_FAIL, payload: err.response.data.msg });
     }
   };
 
   // login user
   const login = async (formData) => {
     try {
-      dispatch({ type: LOGIN_SUCCESS, payload: "" });
+      var users = [];
+      // Parse the serialized data back into an aray of objects
+      users = JSON.parse(localStorage.getItem("users")) || [];
+
+      //check for user with email and password
+      let userFound = users.filter(
+        (user) =>
+          user.email === formData.email && user.password === formData.password
+      );
+
+      if (userFound.length === 0) {
+        alert("Invalid Credentials, Try Again !!");
+      } else {
+        delete userFound[0].password;
+
+        dispatch({ type: LOGIN_SUCCESS, payload: userFound[0] });
+      }
     } catch (err) {
       console.log(err);
-      dispatch({ type: LOGIN_FAIL, payload: err.response.data.msg });
+    }
+  };
+
+  // Remove item from later list
+  const removeReadLaterItem = async (email, slug_name) => {
+    try {
+      var users = [];
+      // Parse the serialized data back into an aray of objects
+      users = JSON.parse(localStorage.getItem("users")) || [];
+
+      //check for user with email
+      var readLaterList = [];
+      users = users.map((user) => {
+        if (user.email === email) {
+          readLaterList = [...user.readLaterList];
+
+          //remove item form reader list
+          user.readLaterList.forEach((item, index) => {
+            if (item.slug_name !== slug_name) {
+              readLaterList.splice(index, 1);
+            }
+          });
+
+          //update readLaterList
+          user.readLaterList = [...readLaterList];
+        }
+
+        return user;
+      });
+
+      // Re-serialize the array back into a string and store it in localStorage
+      localStorage.setItem("users", JSON.stringify(users));
+
+      dispatch({
+        type: REMOVE_READ_LATER_SUCCESS,
+        payload: readLaterList,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  // add item to later list
+  const addReadLaterItem = async (email, article) => {
+    try {
+      var users = [];
+      // Parse the serialized data back into an aray of objects
+      users = JSON.parse(localStorage.getItem("users")) || [];
+
+      //check for user with email
+      var updatedUser = {};
+      users = users.map((user) => {
+        if (user.email === email) {
+          user = { ...user, readLaterList: { ...user.readLaterList, article } };
+        }
+        updatedUser = { ...user };
+        return user;
+      });
+
+      // Re-serialize the array back into a string and store it in localStorage
+      localStorage.setItem("users", JSON.stringify(users));
+
+      dispatch({
+        type: ADD_READ_LATER_SUCCESS,
+        payload: updatedUser,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  // Edit user
+  const updateUserProfile = async (formData) => {
+    console.log(formData);
+
+    try {
+      var users = [];
+      // Parse the serialized data back into an aray of objects
+      users = JSON.parse(localStorage.getItem("users")) || [];
+      var updatedUser;
+      users = users.map((user) => {
+        if (user.email === formData.email) {
+          updatedUser = [user];
+          updatedUser.password = formData.password;
+          updatedUser.name = formData.name;
+
+          return updatedUser;
+        } else {
+          return user;
+        }
+      });
+
+      // Re-serialize the array back into a string and store it in localStorage
+      localStorage.setItem("users", JSON.stringify(users));
+
+      delete updatedUser.password;
+      dispatch({ type: PROFILE_UPDATE_SUCCESS, payload: updatedUser });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  // delete user
+  const deleteAccount = async (email) => {
+    console.log(email);
+
+    try {
+      var users = [];
+      // Parse the serialized data back into an aray of objects
+      users = JSON.parse(localStorage.getItem("users")) || [];
+
+      users = users.filter((user) => user.email !== email);
+
+      // Re-serialize the array back into a string and store it in localStorage
+      localStorage.setItem("users", JSON.stringify(users));
+
+      dispatch({ type: ACCOUNT_DELETE_SUCCESS });
+    } catch (err) {
+      console.log(err);
     }
   };
 
@@ -62,7 +200,21 @@ const UserState = (props) => {
   };
 
   return (
-    <UserContext.Provider value={{}}>{props.children}</UserContext.Provider>
+    <UserContext.Provider
+      value={{
+        user: state.user,
+        currentReadLaterPage: state.currentReadLaterPage,
+        login,
+        logout,
+        register,
+        updateUserProfile,
+        deleteAccount,
+        removeReadLaterItem,
+        addReadLaterItem,
+      }}
+    >
+      {props.children}
+    </UserContext.Provider>
   );
 };
 
--- src/context/user/userReducer.js
+++ src/context/user/userReducer.js
@@ -1,5 +1,34 @@
+import {
+  REGISTER_SUCCESS,
+  LOGIN_SUCCESS,
+  LOGOUT,
+  PROFILE_UPDATE_SUCCESS,
+  ACCOUNT_DELETE_SUCCESS,
+  REMOVE_READ_LATER_SUCCESS,
+  ADD_READ_LATER_SUCCESS
+} from "../types";
+
 const userReducer = (state, action) => {
   switch (action.type) {
+    case REGISTER_SUCCESS:
+    case LOGIN_SUCCESS:
+    case PROFILE_UPDATE_SUCCESS:
+      case ADD_READ_LATER_SUCCESS:
+      return {
+        ...state,
+        user: action.payload,
+      };
+    case LOGOUT:
+    case ACCOUNT_DELETE_SUCCESS:
+      return {
+        ...state,
+        user: null,
+      };
+    case REMOVE_READ_LATER_SUCCESS:
+      return {
+        ...state,
+        user = {...user, readLaterList:{...action.payload}},
+      };
     default:
       return state;
   }
